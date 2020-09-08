c5aec5abb6b541afbf22bec2d024022ff0746652
nonoLin <denkanono@gmail.com>
2020-09-08 15:32:56 +0800

    add restaurants & admin restaurants pages

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const app = express()
-const PORT = 3000
+const port = 3000
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
-
-app.get('/', (req, res) => {
-  res.render('index')
+app.listen(port, () => {
+  console.log(`running on localhost:${port}`)
 })
 
-
-app.listen(PORT, () => {
-  console.log(`running on localhost:${PORT}`)
-})
+require('./routes')(app)
