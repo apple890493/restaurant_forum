211f50d790f27314b0f065a00512782518c8134a
nonoLin <denkanono@gmail.com>
2020-09-08 16:39:12 +0800

    create sequelize

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const db = require('./models')
 const app = express()
 const port = 3000
 
@@ -7,6 +8,7 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.listen(port, () => {
+  db.sequelize.sync()
   console.log(`running on localhost:${port}`)
 })
 
