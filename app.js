faa8576d383c15e6ed5f2981b63223e4e9909f88
nonoLin <denkanono@gmail.com>
2020-09-09 13:04:20 +0800

    confirm user password and unique email flash

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const exphbs = require('express-handlebars')
+const flash = require('connect-flash')
+const session = require('express-session')
 const db = require('./models')
 const { urlencoded } = require('body-parser')
 const app = express()
@@ -9,7 +11,18 @@ const port = 3000
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(session({
+  secret: 'ThisIsSecret',
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(flash())
 
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 
 app.listen(port, () => {
   db.sequelize.sync()
