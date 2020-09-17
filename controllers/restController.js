4b18f2d701cd230255e1e18f3457bfe5b45e6281
nonoLin <denkanono@gmail.com>
2020-09-17 08:38:58 +0800

    add restaurants show page

--- controllers/restController.js
+++ controllers/restController.js
@@ -7,7 +7,6 @@ const restController = {
   getRestaurants: (req, res) => {
     Restaurant.findAll({ include: Category })
       .then(restaurants => {
-        console.log(restaurants)
         const data = restaurants.map(r => ({
           ...r.dataValues,
           description: r.dataValues.description.substring(0, 50),
@@ -15,6 +14,17 @@ const restController = {
         }))
         return res.render('restaurants', {
           restaurants: data
+        })
+      })
+  },
+
+  getRestaurant: (req, res) => {
+    return Restaurant.findByPk(req.params.id, {
+      include: Category
+    })
+      .then(restaurant => {
+        return res.render('restaurant', {
+          restaurant: restaurant.toJSON()
         })
       })
   }
